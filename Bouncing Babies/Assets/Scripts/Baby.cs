0e3defce0cc612d93835eacb48ebdd020a03e8e5
tomersh9 <62946603+tomersh9@users.noreply.github.com>
2021-01-28 22:02:51 +0200

    CleanUp

--- Bouncing Babies/Assets/Scripts/Baby.cs
+++ Bouncing Babies/Assets/Scripts/Baby.cs
@@ -4,11 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Baby : MonoBehaviour {
-    
     [SerializeField] private float pushForce = 5f;
     [SerializeField] private float pushX = 2.5f;
     [SerializeField] private float pushy = 2.5f;
-    
+
     private Rigidbody2D _rigidbody2D;
 
     private void Awake() {
@@ -22,7 +21,7 @@ public class Baby : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Trampoline")) {
-            _rigidbody2D.velocity += new Vector2(pushX,pushy);
+            _rigidbody2D.velocity += new Vector2(pushX, pushy);
             AudioManager.GetInstance().PlayBabyJumpSfx();
         }
     }
--- Bouncing Babies/Assets/Scripts/Player.cs
+++ Bouncing Babies/Assets/Scripts/Player.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     [SerializeField] private float moveSpeed = 2f;
 
-    private float minX, maxX;
+    private float _minX, _maxX;
 
     private Camera _mainCam;
-    private Vector2 moveToPosition;
+    private Vector2 _moveToPosition;
 
     private void Awake() {
         _mainCam = Camera.main;
@@ -21,8 +21,8 @@ public class Player : MonoBehaviour {
     }
 
     private void SetupMoveBoundries() {
-        minX = _mainCam.ViewportToWorldPoint(new Vector3(0.15f, 0, 0)).x;
-        maxX = _mainCam.ViewportToWorldPoint(new Vector3(0.8f, 0, 0)).x;
+        _minX = _mainCam.ViewportToWorldPoint(new Vector3(0.15f, 0, 0)).x;
+        _maxX = _mainCam.ViewportToWorldPoint(new Vector3(0.8f, 0, 0)).x;
     }
 
     private void HandleMovement() {
@@ -30,23 +30,12 @@ public class Player : MonoBehaviour {
             Vector2 touchPosition = _mainCam.ScreenToWorldPoint(Input.mousePosition);
             float deltaX = touchPosition.x * moveSpeed * Time.deltaTime;
             float newPositionX = deltaX + transform.position.x;
-            if (newPositionX >= minX && newPositionX <= maxX) {
-                moveToPosition.x = newPositionX;
-                moveToPosition.y = transform.position.y;
-                transform.position = moveToPosition;
+            if (newPositionX >= _minX && newPositionX <= _maxX) {
+                _moveToPosition.x = newPositionX;
+                _moveToPosition.y = transform.position.y;
+                transform.position = _moveToPosition;
             }
         }
-        /*if (Input.touchCount > 0) {
-            Touch touch = Input.GetTouch(0);
-            Vector2 touchPosition = _mainCam.ScreenToWorldPoint(touch.position);
-            float newPosX = transform.position.x + touchPosition.x;
-            print(newPosX);
-            if (touchPosition.x > minX && touchPosition.x < maxX) {
-                Vector2 moveToPosition = Vector2.MoveTowards(transform.position, touchPosition, moveSpeed * Time.deltaTime);
-                moveToPosition.y = transform.position.y; //keep the same Y all the time
-                transform.position = moveToPosition; //update to new position
-            }
-        }*/
     }
 
 }
